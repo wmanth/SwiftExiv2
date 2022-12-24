57998e971641070d400d394bb529c5f849da99e9
Wolfram Manthey <github@wmanth.net>
2022-12-24 22:45:55 +0800

    Added Exiv2 v0.27.5 library as submodule

--- Package.swift
+++ Package.swift
@@ -19,10 +19,23 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
+            name: "exiv2",
+            sources: [
+                "library/src",
+                "library/xmpsdk/src"],
+            cxxSettings: [
+                .headerSearchPath("library/include"),
+                .headerSearchPath("library/include/exiv2"),
+                .headerSearchPath("library/xmpsdk/include")]),
+        .target(
             name: "SwiftExiv2",
-            dependencies: []),
+            dependencies: ["exiv2"]),
         .testTarget(
             name: "SwiftExiv2Tests",
-            dependencies: ["SwiftExiv2"]),
+            dependencies: ["SwiftExiv2"],
+            linkerSettings: [
+                .linkedLibrary("expat"),
+                .linkedLibrary("iconv"),
+                .linkedLibrary("z")])
     ]
 )
