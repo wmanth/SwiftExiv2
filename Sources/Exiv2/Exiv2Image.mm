1a7ae16a81fb54c2bbc6a572d143746252cc65d8
Wolfram Manthey <github@wmanth.net>
2022-12-28 16:19:05 +0800

    Implemented GPS latitude/longitude tags

--- Sources/Exiv2/Exiv2Image.mm
+++ Sources/Exiv2/Exiv2Image.mm
@@ -1,22 +1,103 @@
-#import "Exiv2Image.h"
+#import "Exiv2Image.hpp"
 
-#include "exiv2/exiv2.hpp"
-
-@interface Exiv2Image ()
-
-@property Exiv2::Image::AutoPtr image_ptr;
-
-@end
+const char* const kGPSLatitude      = "Exif.GPSInfo.GPSLatitude";
+const char* const kGPSLatitudeRef   = "Exif.GPSInfo.GPSLatitudeRef";
+const char* const kGPSLongitude     = "Exif.GPSInfo.GPSLongitude";
+const char* const kGPSLongitudeRef  = "Exif.GPSInfo.GPSLongitudeRef";
 
 @implementation Exiv2Image
 
-- (instancetype)initWithImage:(Exiv2::Image::AutoPtr)image {
-    self.image_ptr = image;
+- (instancetype)initWithPath:(NSString*)path {
+    if (self = [super init]) {
+        const char* imagePath = [path cStringUsingEncoding:NSUTF8StringEncoding];
+        Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(imagePath);
+        _image_ptr = image;
+    }
+    return self;
 }
 
-+ (Exiv2Image*)imageFromBuffer:(const char*)data size:(long)size {
-    Exiv2::Image::AutoPtr image = Exiv2::ImageFactory::open(data, size);
-    return [[Exiv2Image alloc] initWithImage:image];
+- (void)readMetadata {
+    _image_ptr->readMetadata();
+}
+
+- (void)writeMetadata {
+    _image_ptr->writeMetadata();
+}
+
+- (nullable NSNumber *)dmsToDouble:(Exiv2::URationalValue *)rValue negative:(BOOL)negative {
+    if (rValue == NULL || rValue->count() != 3) { return NULL; }
+
+    Exiv2::Rational rDeg = rValue->toRational(0);
+    Exiv2::Rational rMin = rValue->toRational(1);
+    Exiv2::Rational rSec = rValue->toRational(2);
+
+    double deg = (double)rDeg.first/rDeg.second;
+    double min = (double)rMin.first/rMin.second;
+    double sec = (double)rSec.first/rSec.second;
+
+    int sign = negative ? -1 : 1;
+    return [NSNumber numberWithDouble:sign * ((sec/60 + min)/60 + deg)];
+}
+
+- (Exiv2::URationalValue)doubleToDms:(double)value {
+    int deg = (int)value;
+        value -= deg;
+        value *= 60;
+    int min = (int)value;
+        value -= min;
+        value *= 60;
+    int sec = (int)(value * 1000);
+
+    Exiv2::URationalValue rv;
+    rv.value_.push_back(std::make_pair(deg, 1));
+    rv.value_.push_back(std::make_pair(min, 1));
+    rv.value_.push_back(std::make_pair(sec, 1000));
+
+    return rv;
+}
+
+- (Exiv2::Value::AutoPtr)valueForExifKey:(const char *)keyName {
+    Exiv2::ExifData& exifData = _image_ptr->exifData();
+    Exiv2::ExifKey key(keyName);
+    Exiv2::ExifData::iterator pos = exifData.findKey(key);
+    if (pos == exifData.end()) { return Exiv2::Value::AutoPtr(NULL); }
+    return pos->getValue();
+}
+
+- (Exiv2::AsciiValue*)asciiValueForExifKey:(const char *)keyName {
+    Exiv2::Value::AutoPtr value = [self valueForExifKey:keyName];
+    return dynamic_cast<Exiv2::AsciiValue*>(value.release());
+}
+
+- (Exiv2::URationalValue*)rationalValueForExifKey:(const char *)keyName {
+    Exiv2::Value::AutoPtr value = [self valueForExifKey:keyName];
+    return dynamic_cast<Exiv2::URationalValue*>(value.release());
+}
+
+- (NSNumber *)getLatitude {
+    Exiv2::AsciiValue* latRef = [self asciiValueForExifKey:kGPSLatitudeRef];
+    Exiv2::URationalValue* latVal = [self rationalValueForExifKey:kGPSLatitude];
+    return (latRef && latVal) ?
+        [self dmsToDouble:latVal negative:(latRef->toString() == "S")] : NULL;
+}
+
+- (NSNumber *)getLongitude {
+    Exiv2::AsciiValue* lonRef = [self asciiValueForExifKey:kGPSLongitudeRef];
+    Exiv2::URationalValue* lonVal = [self rationalValueForExifKey:kGPSLongitude];
+    return (lonRef && lonVal) ?
+        [self dmsToDouble:lonVal negative:lonRef->toString() == "W"] : NULL;
+}
+
+- (void)setLatitude:(NSNumber *)latitude {
+    Exiv2::ExifData& exifData = _image_ptr->exifData();
+    exifData[kGPSLatitude] = [self doubleToDms:abs(latitude.doubleValue)];
+    exifData[kGPSLatitudeRef] = latitude.doubleValue > 0 ? "N" : "S";
+}
+
+- (void)setLongitude:(NSNumber *)longitude {
+    Exiv2::ExifData& exifData = _image_ptr->exifData();
+    exifData[kGPSLongitude] = [self doubleToDms:abs(longitude.doubleValue)];
+    exifData[kGPSLongitudeRef] = longitude.doubleValue > 0 ? "E" : "W";
 }
 
 @end
--- Tests/SwiftExiv2Tests/SwiftExiv2Tests.swift
+++ Tests/SwiftExiv2Tests/SwiftExiv2Tests.swift
@@ -1,11 +1,68 @@
 import XCTest
-@testable import SwiftExiv2
+
+@testable import Exiv2
 
 final class SwiftExiv2Tests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(SwiftExiv2().text, "Hello, World!")
+
+    private var temporaryDirectoryURL: URL!
+
+    private var testImageURL: URL! {
+        Bundle.module.url(forResource: "test",
+                        withExtension: "jpg",
+                         subdirectory: "Assets")
+    }
+
+    override func setUp() {
+        temporaryDirectoryURL = FileManager.default.temporaryDirectory
+            .appendingPathComponent(UUID().uuidString)
+        do {
+            try FileManager.default.createDirectory(at: temporaryDirectoryURL,
+                           withIntermediateDirectories: true);
+        } catch {
+            XCTFail("could not create temporary folder")
+        }
+    }
+
+    override func tearDown() {
+        do {
+            try FileManager.default.removeItem(at: temporaryDirectoryURL)
+        } catch {
+            XCTFail("could not remove temporary folder")
+        }
+    }
+
+    func testFailToReadLatLon() throws {
+        let image = Exiv2Image(path: testImageURL.path)
+        image.readMetadata()
+
+        let lat = image.getLatitude()
+        let lon = image.getLongitude()
+
+        XCTAssertNil(lat)
+        XCTAssertNil(lon)
+    }
+
+    func testSuccessToWriteLatLon() throws {
+        let destURL = temporaryDirectoryURL.appendingPathComponent(testImageURL.lastPathComponent)
+        try FileManager.default.copyItem(at: testImageURL, to: destURL)
+        let image = Exiv2Image(path: destURL.path)
+
+        let testLat = NSNumber(31.22896)
+        let testLon = NSNumber(121.48022)
+
+        image.readMetadata()
+        image.setLatitude(testLat)
+        image.setLongitude(testLon)
+        image.writeMetadata()
+
+        let result = Exiv2Image(path: destURL.path)
+        result.readMetadata()
+        if let lat = image.getLatitude(),
+           let lon = image.getLongitude() {
+            XCTAssertEqual(lat, testLat)
+            XCTAssertEqual(lon, testLon)
+        } else {
+            XCTFail()
+        }
     }
 }
