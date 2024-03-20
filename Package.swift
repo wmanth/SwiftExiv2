83afa99c7c4cdc25b69e07cf6564c0d606ad2c19
Wolfram Manthey <github@wmanth.net>
2024-03-21 00:30:03 +0800

    Changed submodule location

--- Package.swift
+++ Package.swift
@@ -19,26 +19,17 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "exiv2lib",
-            exclude: [
-                "library/src/exiv2.cpp",
-                "library/src/actions.cpp",
-                "library/src/getopt.cpp",
-                "library/src/utils.cpp"],
-            sources: [
-                "library/src",
-                "library/xmpsdk/src"],
-            cxxSettings: [
-                .headerSearchPath("library/include/exiv2"),
-                .headerSearchPath("library/xmpsdk/include"),
-                .unsafeFlags([
-                    "-Wno-shorten-64-to-32",
-                    "-Wno-unused-command-line-argument"])]),
-        .target(
             name: "Exiv2",
-            dependencies: ["exiv2lib"],
+            exclude: [
+                "exiv2/src/utils.cpp",
+                "exiv2/xmpsdk/src/UnicodeInlines.incl_cpp"],
+            sources: [
+                "exiv2/src",
+                "exiv2/xmpsdk/src"],
             cxxSettings: [
-                .headerSearchPath("../exiv2lib/library/include")]),
+                .headerSearchPath("include"),
+                .headerSearchPath("exiv2/include/exiv2"),
+                .headerSearchPath("exiv2/xmpsdk/include")]),
         .testTarget(
             name: "SwiftExiv2Tests",
             dependencies: ["Exiv2"],
