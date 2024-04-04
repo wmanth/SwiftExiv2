d527a590756dc989e34494a7f651056cfa68e6bd
Wolfram Manthey <github@wmanth.net>
2024-04-05 00:52:46 +0800

    Make things public

--- Sources/SwiftExiv2/Image.swift
+++ Sources/SwiftExiv2/Image.swift
@@ -1,19 +1,19 @@
 import Foundation
 import CxxExiv2
 
-class Image {
-    let url: URL
+public class Image {
+    public let url: URL
     private var imageProxy: ImageProxy
 
-    init(url: URL) {
+    public init(url: URL) {
         self.url = url
         imageProxy = ImageProxy(std.string(url.absoluteString))
     }
 
-    func readMetadata() { imageProxy.readMetadata() }
-    func writeMetadata() { imageProxy.writeMetadata() }
+    public func readMetadata() { imageProxy.readMetadata() }
+    public func writeMetadata() { imageProxy.writeMetadata() }
 
-    var dateTimeOriginal: DateComponents? {
+    public var dateTimeOriginal: DateComponents? {
         set(newValue) {
             if let timeStamp = newValue?.toTimeStamp() {
                 imageProxy.setDateTimeOriginal(timeStamp)
@@ -26,7 +26,7 @@ class Image {
         }
     }
 
-    var latitude: Double? {
+    public var latitude: Double? {
         set(newValue) {
             if let latitude = newValue { imageProxy.setLatitude(latitude) }
             else { imageProxy.removeLatitude() }
@@ -34,7 +34,7 @@ class Image {
         get { imageProxy.getLatitude().value }
     }
 
-    var longitude: Double? {
+    public var longitude: Double? {
         set(newValue) {
             if let longitude = newValue { imageProxy.setLongitude(longitude) }
             else { imageProxy.removeLongitude() }
@@ -42,7 +42,7 @@ class Image {
         get { imageProxy.getLongitude().value }
     }
 
-    var altitude: Float? {
+    public var altitude: Float? {
         set(newValue) {
             if let altitude = newValue { imageProxy.setAltitude(altitude) }
             else { imageProxy.removeAltitude() }
